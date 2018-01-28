8f72f141f84601a90dee53ec9c9f2fd236c9d6ff
Mateo Velenik <mateo.velenik@gmail.com>
2018-01-28 13:46:32 +0100

    added multi line label control

---
+++ SSWS.Mobile/SSWS.Mobile.Android/Renderers/CustomMultiLineLabelRenderer.cs
@@ -0,0 +1,24 @@
+﻿using SSWS.Mobile.Controls;
+using SSWS.Mobile.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MultiLineLabel), typeof(CustomMultiLineLabelRenderer))]
+namespace SSWS.Mobile.Droid.Renderers
+{
+    public class CustomMultiLineLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            MultiLineLabel multiLineLabel = (MultiLineLabel)Element;
+
+            if (multiLineLabel != null && multiLineLabel.Lines != -1)
+            {
+                Control.SetSingleLine(false);
+                Control.SetLines(multiLineLabel.Lines);
+            }
+        }
+    }
+}
---
+++ SSWS.Mobile/SSWS.Mobile.iOS/Renderers/CustomMultiLineLabelRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+using SSWS.Mobile.Controls;
+using SSWS.Mobile.iOS.Renderers;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MultiLineLabel), typeof(CustomMultiLineLabelRenderer))]
+namespace SSWS.Mobile.iOS.Renderers
+{
+
+    public class CustomMultiLineLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            MultiLineLabel multiLineLabel = (MultiLineLabel)Element;
+
+            if (multiLineLabel != null && multiLineLabel.Lines != -1)
+                Control.Lines = multiLineLabel.Lines;
+        }
+    }
+}
