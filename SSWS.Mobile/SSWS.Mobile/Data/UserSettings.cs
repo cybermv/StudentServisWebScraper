0708bffdd0672993f674ac3193352ff06a28d3e9
Mateo Velenik <mateo.velenik@gmail.com>
2018-02-03 15:36:09 +0100

    added settings page

---
+++ SSWS.Mobile/SSWS.Mobile/Data/UserSettings.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SSWS.Mobile.Data
+{
+    public class UserSettings
+    {
+        public const string ApplicationPropertiesKey = "SSWS.UserSettings";
+
+        private UserSettings()
+        {
+        }
+
+        public DateTime LastRefreshDate { get; set; }
+
+        public List<int> SelectedCategories { get; set; }
+
+        public decimal? MinHourlyRate { get; set; }
+
+        public bool ShowNonParsedJobs { get; set; }
+
+        public bool ShowNotifications { get; set; }
+
+        /// <summary>
+        /// The default settings that will be used on first application start
+        /// </summary>
+        private static UserSettings GetInitialSettings()
+        {
+            return new UserSettings
+            {
+                LastRefreshDate = new DateTime(1993, 7, 6),
+                SelectedCategories = new List<int>(0),
+                MinHourlyRate = 10, // LOL
+                ShowNonParsedJobs = true,
+                ShowNotifications = true
+            };
+        }
+
+        public static UserSettings Load()
+        {
+            IDictionary<string, object> storage = Application.Current.Properties;
+
+            if (!storage.ContainsKey(ApplicationPropertiesKey))
+            {
+                UserSettings firstSettings = GetInitialSettings();
+                Store(firstSettings);
+                return firstSettings;
+            }
+
+            string settingsJson = (string)storage[ApplicationPropertiesKey];
+            UserSettings settings = JsonConvert.DeserializeObject<UserSettings>(settingsJson);
+
+            return settings;
+        }
+
+        public static void Store(UserSettings settings)
+        {
+            IDictionary<string, object> storage = Application.Current.Properties;
+            string settingsJson = JsonConvert.SerializeObject(settings);
+
+            storage[ApplicationPropertiesKey] = settingsJson;
+        }
+    }
+}
---
+++ SSWS.Mobile/SSWS.Mobile/Views/Settings.xaml.cs
@@ -0,0 +1,20 @@
+﻿using SSWS.Mobile.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SSWS.Mobile.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Settings : ContentPage
+	{
+        public UserSettings UserSettings { get; set; }
+
+        public Settings()
+		{
+			InitializeComponent();
+
+            UserSettings = UserSettings.Load();
+            this.BindingContext = this;
+		}
+	}
+}
